67ed647cbc7eb58a62838528cebdfdfde054fa71
lcasserly <luciacasserly@gmail.com>
2025-07-21 23:47:48 -0300

    .env

--- src/models/data.js
+++ src/models/data.js
@@ -4,13 +4,13 @@ import {getFirestore} from "firebase/firestore";
 
 //Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDiU_5XP0O4nhxbpvjWzTCmVKNS-lcy_Nk",
-  authDomain: "proyectofinalnodejs-casserly.firebaseapp.com",
-  projectId: "proyectofinalnodejs-casserly",
-  storageBucket: "proyectofinalnodejs-casserly.firebasestorage.app",
-  messagingSenderId: "463273704940",
-  appId: "1:463273704940:web:c0769704c3c071977d54c9"
-};
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+ };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
